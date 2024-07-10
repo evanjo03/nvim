1f8480e3f7e24c72014d18b8731b5275342c7bb5
John Evans <john.wes.evans@gmail.com>
2024-07-10 12:57:48 -0500

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,32 @@
+require 'evanjo03.config.core'
+require 'evanjo03.config.autocmds'
+require 'evanjo03.config.keymaps'
+require 'evanjo03.config.options'
+require 'evanjo03.config.lazy'
+
+require('lazy').setup({
+  { import = 'evanjo03.plugins' },
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
