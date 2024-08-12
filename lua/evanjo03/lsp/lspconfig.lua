0dcae650243175eb71b0d1ea7547f0cd6e48954a
John Evans <john.wes.evans@gmail.com>
2024-08-11 19:11:13 -0500

    Updates

--- lua/evanjo03/lsp/lspconfig.lua
+++ lua/evanjo03/lsp/lspconfig.lua
@@ -197,6 +197,11 @@ return { -- LSP Configuration & Plugins
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'bashls',
+      'html',
+      'jsonls',
+      'tsserver',
+      'volar',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
